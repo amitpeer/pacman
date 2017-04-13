25f0536a858bba8ac3bb2dd97112451d3bb053d5
amitp <amitpeer05@gmail.com>
2017-04-13 14:47:24 +0300

    Added register validation and re-designed form in table

--- actions.js
+++ actions.js
@@ -25,11 +25,7 @@ $(document).ready(function () {
             $("#loginDiv").show();
         }
     });
-
-
-
-
-
+    
 
 /*
 
@@ -46,25 +42,6 @@ $(document).ready(function () {
     //initialize birthday input in the register form
 
     //Year:
-    var d = new Date();
-    var n = d.getFullYear();
-    for (var i = n; i >= 1900; i--) {
-        var opt = new Option();
-        opt.value = opt.text = i;
-        $('[name="year"]')[0].add(opt);
-    }
-    //Month:
-    for (var i = 12; i >= 1; i--) {
-        var opt = new Option();
-        opt.value = opt.text = i;
-        $('[name="month"]')[0].add(opt);
-    }
-    //Day:
-    for (var i = 31; i >= 1; i--) {
-        var opt = new Option();
-        opt.value = opt.text = i;
-        $('[name="day"]')[0].add(opt);
-    }
 });
 
 function pressedRegister(){
